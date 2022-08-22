1e9aa8663ff446e7cf8e0c40b02f96af3f9ebadc
shin-ye-rang <you@example.com>
2022-08-22 10:00:20 +0900

    프로젝트 최초 공유

---
+++ zproject00/src/main/java/com/example/demo/Zproject00Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zproject00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zproject00Application.class, args);
+	}
+
+}
